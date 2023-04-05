c6449688c4fc16ba6dcfd11f00c323b7e61414ef
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-05 17:23:30 +0200

    Fixing `module not found` error.

--- macropy/__main__.py
+++ macropy/__main__.py
@@ -2,10 +2,11 @@
 
 if __name__ == '__main__':
     import sys, os
-    from scriptController import main_with_profiling, main
     
     #+ Changing the working directory to where this script is.
     os.chdir(os.path.dirname(__file__))
+    
+    from scriptController import main_with_profiling, main
     
     if len(sys.argv) > 1 and sys.argv[1] in ("-p", "--profile", "--prof"):
         main_with_profiling()
